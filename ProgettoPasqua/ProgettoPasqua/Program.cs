200904116364527ac2b886bc314c988c5268435d
BARIN FRANCESCO <12999355@itisrossi.edu.it>
2026-03-03 10:30:09 +0100

    Creazione uova

---
+++ ProgettoPasqua/ProgettoPasqua/Program.cs
@@ -0,0 +1,11 @@
+﻿using LMyDataStructures;
+namespace ProgettoPasqua
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ ProgettoPasqua/ProgettoPasqua/Uovo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LMyDataStructures;
+
+namespace ProgettoPasqua
+{
+    public class Uovo
+    {
+
+        private static int count = 0;
+
+        public int ID { get; private set; }
+        public Colori FirstColor { get; private set; }
+        public Colori SecondColor { get; private set; }
+
+        public Uovo()
+        {
+            ID = count;
+            count++;
+            GiveColor();
+        }
+
+        public void GiveColor()
+        {
+            FirstColor = (Colori)Random.Shared.Next(0, Enum.GetValues<Colori>().Length); //sceglie un valore random tra 0 e il massimo numero, e lo "casta" in enum
+            SecondColor = (Colori)Random.Shared.Next(0, Enum.GetValues<Colori>().Length);
+        }
+    }
+}
